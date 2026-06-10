222ca8c18126e0ead4366920c8125a27d299c2e0
krithik d shetty <shettykrithik150@gmail.com>
2026-06-10 20:12:32 +0530

    Updated graphics editor with modify feature

--- KRITHIK_D_SHETTTY_R25EJ056.c
+++ KRITHIK_D_SHETTTY_R25EJ056.c
@@ -12,7 +12,7 @@ void initCanvas()
 {
     for(int i=0;i<ROWS;i++)
         for(int j=0;j<COLS;j++)
-            canvas[i][j]=' ';
+            canvas[i][j]='_';
 }
 
 /* Display Canvas */
@@ -55,6 +55,12 @@ void drawLine(int x1,int y1,int x2,int y2,char ch)
     int dy=y2-y1;
 
     int steps=abs(dx)>abs(dy)?abs(dx):abs(dy);
+
+    if(steps==0)
+    {
+        plot(x1,y1,ch);
+        return;
+    }
 
     float xInc=(float)dx/steps;
     float yInc=(float)dy/steps;
@@ -111,7 +117,7 @@ void drawCircle(int xc,int yc,int r,char ch)
     }
 }
 
-/* Clear Picture */
+/* Clear Canvas */
 void clearCanvas()
 {
     initCanvas();
@@ -134,9 +140,13 @@ int main()
         printf("6. Delete Rectangle\n");
         printf("7. Delete Triangle\n");
         printf("8. Delete Circle\n");
-        printf("9. Display Picture\n");
-        printf("10. Clear Picture\n");
-        printf("11. Exit\n");
+        printf("9. Modify Line\n");
+        printf("10. Modify Rectangle\n");
+        printf("11. Modify Triangle\n");
+        printf("12. Modify Circle\n");
+        printf("13. Display Picture\n");
+        printf("14. Clear Picture\n");
+        printf("15. Exit\n");
 
         printf("Enter Choice: ");
         scanf("%d",&choice);
@@ -146,6 +156,7 @@ int main()
             case 1:
             {
                 int x1,y1,x2,y2;
+
                 printf("Enter x1 y1 x2 y2: ");
                 scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
 
@@ -156,10 +167,11 @@ int main()
             case 2:
             {
                 int x,y,w,h;
+
                 printf("Enter x y width height: ");
                 scanf("%d%d%d%d",&x,&y,&w,&h);
 
-                drawRectangle(x,y,w,h,'#');
+                drawRectangle(x,y,w,h,'*');
                 break;
             }
 
@@ -176,7 +188,7 @@ int main()
                 printf("Enter x3 y3: ");
                 scanf("%d%d",&x3,&y3);
 
-                drawTriangle(x1,y1,x2,y2,x3,y3,'+');
+                drawTriangle(x1,y1,x2,y2,x3,y3,'*');
                 break;
             }
 
@@ -187,27 +199,29 @@ int main()
                 printf("Enter center x y and radius: ");
                 scanf("%d%d%d",&xc,&yc,&r);
 
-                drawCircle(xc,yc,r,'O');
+                drawCircle(xc,yc,r,'*');
                 break;
             }
 
             case 5:
             {
                 int x1,y1,x2,y2;
+
                 printf("Enter x1 y1 x2 y2: ");
                 scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
 
-                drawLine(x1,y1,x2,y2,' ');
+                drawLine(x1,y1,x2,y2,'_');
                 break;
             }
 
             case 6:
             {
                 int x,y,w,h;
+
                 printf("Enter x y width height: ");
                 scanf("%d%d%d%d",&x,&y,&w,&h);
 
-                drawRectangle(x,y,w,h,' ');
+                drawRectangle(x,y,w,h,'_');
                 break;
             }
 
@@ -224,7 +238,7 @@ int main()
                 printf("Enter x3 y3: ");
                 scanf("%d%d",&x3,&y3);
 
-                drawTriangle(x1,y1,x2,y2,x3,y3,' ');
+                drawTriangle(x1,y1,x2,y2,x3,y3,'_');
                 break;
             }
 
@@ -235,20 +249,90 @@ int main()
                 printf("Enter center x y and radius: ");
                 scanf("%d%d%d",&xc,&yc,&r);
 
-                drawCircle(xc,yc,r,' ');
+                drawCircle(xc,yc,r,'_');
                 break;
             }
 
             case 9:
+            {
+                int ox1,oy1,ox2,oy2;
+                int nx1,ny1,nx2,ny2;
+
+                printf("Old x1 y1 x2 y2: ");
+                scanf("%d%d%d%d",&ox1,&oy1,&ox2,&oy2);
+
+                drawLine(ox1,oy1,ox2,oy2,'_');
+
+                printf("New x1 y1 x2 y2: ");
+                scanf("%d%d%d%d",&nx1,&ny1,&nx2,&ny2);
+
+                drawLine(nx1,ny1,nx2,ny2,'*');
+                break;
+            }
+
+            case 10:
+            {
+                int ox,oy,ow,oh;
+                int nx,ny,nw,nh;
+
+                printf("Old x y width height: ");
+                scanf("%d%d%d%d",&ox,&oy,&ow,&oh);
+
+                drawRectangle(ox,oy,ow,oh,'_');
+
+                printf("New x y width height: ");
+                scanf("%d%d%d%d",&nx,&ny,&nw,&nh);
+
+                drawRectangle(nx,ny,nw,nh,'*');
+                break;
+            }
+
+            case 11:
+            {
+                int ox1,oy1,ox2,oy2,ox3,oy3;
+                int nx1,ny1,nx2,ny2,nx3,ny3;
+
+                printf("Old Triangle Points (x1 y1 x2 y2 x3 y3): ");
+                scanf("%d%d%d%d%d%d",
+                      &ox1,&oy1,&ox2,&oy2,&ox3,&oy3);
+
+                drawTriangle(ox1,oy1,ox2,oy2,ox3,oy3,'_');
+
+                printf("New Triangle Points (x1 y1 x2 y2 x3 y3): ");
+                scanf("%d%d%d%d%d%d",
+                      &nx1,&ny1,&nx2,&ny2,&nx3,&ny3);
+
+                drawTriangle(nx1,ny1,nx2,ny2,nx3,ny3,'*');
+                break;
+            }
+
+            case 12:
+            {
+                int oxc,oyc,orad;
+                int nxc,nyc,nrad;
+
+                printf("Old center x y radius: ");
+                scanf("%d%d%d",&oxc,&oyc,&orad);
+
+                drawCircle(oxc,oyc,orad,'_');
+
+                printf("New center x y radius: ");
+                scanf("%d%d%d",&nxc,&nyc,&nrad);
+
+                drawCircle(nxc,nyc,nrad,'*');
+                break;
+            }
+
+            case 13:
                 displayCanvas();
                 break;
 
-            case 10:
+            case 14:
                 clearCanvas();
                 printf("Canvas Cleared!\n");
                 break;
 
-            case 11:
+            case 15:
                 printf("Exiting...\n");
                 exit(0);
 
